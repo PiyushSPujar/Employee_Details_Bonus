7385af98a45cb139b6fd6c6833ab7e1d938d6084
Piyush <01fe22bca214@kletech.ac.in>
2024-03-31 21:55:52 +0530

    Pushing the code

---
+++ EmployeeDetailsDisplay.java
@@ -0,0 +1,27 @@
+public class EmployeeDetailsDisplay extends BonusDisplay  {
+
+    String employeeName;
+    int employeeID;
+    static double employeeSalary;
+
+        public EmployeeDetailsDisplay(String employeeName,int employeeID,double employeeSalary){
+       this.employeeName = employeeName;
+       this.employeeID = employeeID;
+       this.employeeSalary = employeeSalary;
+        }
+
+        public void displayDetails(){
+            System.out.println("\nEmployee Details:");
+            System.out.println("Name: " + employeeName);
+            System.out.println("ID: " + employeeID);
+            System.out.println("Salary:" + employeeSalary);
+       }
+       public static void main(String[] args) {
+            EmployeeDetailsDisplay emp = new EmployeeDetailsDisplay("Piyush",1234,45000);
+            emp.displayDetails();    
+            double x = 8.33;
+            System.out.println("bonusSalary: " +bonusSalary(x, employeeSalary));
+        }
+
+}
+
